8f06cf78a4f87086f3915368ca976e970bb536c9
SOWNDARYA-R7 <kit28.24cs165@gmail.com>
2026-09-18 22:37:38 +0530

    search Conversation

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -28,7 +28,34 @@ const Sidebar = () => {
       {/* Search Conversations*/}
       <div className="flex items-center gap-2 p-3 mt-4 border border-gray-400
       dark:border-white/20 rounded-md">
-        <img src={assets.search_icon} className="inv" alt="" />
+        <img src={assets.search_icon} className="w-4 not-dark:invert" alt="" />
+        <input onChange={(e) => setSearch(e.target.value)} value={search} type="text" 
+        placeholder="Search Conversations" 
+        className="text-xs placeholder:text-grey-400 outline-none"/>
+      </div>
+      {/* Recent Chats */}
+      {chats.length > 0 && <p>Recent Chats</p>}
+      <div className="flex-1 overflow-y-scroll mt-3 text-sm space-y-3">
+        {
+          chats.filter((chat) => chat.messages[0] ? chat.messages[0]?.content.
+          toLowerCase().includes(search.toLowerCase()) : chat.name.toLowerCase().
+          includes(search.toLowerCase())).map((chat) => (
+            <div key={chat._id} className="p-2 px-4 dark-bg-[#57317C]/10 border
+            border-grey-300 dark-border-[#80609F]/15 rounded-md cursor-pointer
+            flex justify-between group">
+              <div>
+                <p className="truncate w-full">
+                  {chat.messages.length > 0 ? chat.messages[0].content.
+                  slice(0,32) : chat.name} 
+                </p>
+                <p className="text-xs text-grey-500 dark:text-[#B1A6C0]">{chat.updatedAt}</p>
+              </div>
+              <img src={assets.bin_icon} className="hidden group-hover:block
+              w-4 cursor-pointer not-dark:invert" alt=""/>
+
+            </div>
+          ))
+        }
       </div>
     </div>
   )
